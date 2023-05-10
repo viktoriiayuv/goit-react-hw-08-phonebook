3136a44c71c4eafd1c0bcd49c7698a721a1fd125
Viktoriia Bieltsova <vikav0206@gmail.com>
2023-05-11 00:20:06 +0300

    delete loader

--- src/pages/Contacts/Contacts.jsx
+++ src/pages/Contacts/Contacts.jsx
@@ -1,15 +1,13 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { fetchContacts } from 'redux/contacts/operations';
 import ContactList from 'components/ContactList/ContactList';
 import ContactForm from 'components/ContactForm/ContactForm';
 import Filter from 'components/Filter/Filter';
-import { selectIsLoading } from 'redux/selectors';
 import { ContactsContainer } from './Contacts.styled';
 
 export default function Contacts() {
   const dispatch = useDispatch();
-  const isLoading = useSelector(selectIsLoading);
 
   useEffect(() => {
     dispatch(fetchContacts());
@@ -22,7 +20,6 @@ export default function Contacts() {
         <ContactForm />
         <div className="filter">
           <Filter />
-          <div>{isLoading && 'Request in progress...'}</div>
           <ContactList />
         </div>
       </ContactsContainer>
